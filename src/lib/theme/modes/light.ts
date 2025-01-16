8a44a3db9f4f030dc341e41413dbd0b19d27784d
Jørgen Kleppan <jokle5828@oslomet.no>
2025-01-16 23:46:49 +0100

    change light colors

--- src/lib/theme/modes/light.ts
+++ src/lib/theme/modes/light.ts
@@ -3,6 +3,6 @@ import { colors } from "../colors";
 export const lightTheme = {
   primary: colors.stone[800],
   secondary: colors.blue[200],
-  background: colors.blue[50],
+  background: colors.green[100],
   text: colors.stone[800],
 };
