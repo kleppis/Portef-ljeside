e7169e17696e999a0aa1442c58d7d16c8369b596
Jørgen Kleppan <jokle5828@oslomet.no>
2025-01-27 14:36:09 +0100

    dark

--- src/lib/theme/modes/dark.ts
+++ src/lib/theme/modes/dark.ts
@@ -2,7 +2,7 @@ import { colors } from "../colors";
 
 export const darkTheme = {
   primary: colors.stone[800], // Lys blå
-  secondary: "#94a3b8", // Lys grå
+  secondary: colors.red[400], // Lys grå
   background: colors.stone[900], // Mørk blå
   text: colors.blue[300],
   divider: colors.stone[500],
