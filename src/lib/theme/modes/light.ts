4a191599cfec80fdf45b7f2e2bae20858ee5cb43
Jørgen Kleppan <jokle5828@oslomet.no>
2025-01-27 14:34:39 +0100

    light

--- src/lib/theme/modes/light.ts
+++ src/lib/theme/modes/light.ts
@@ -2,7 +2,7 @@ import { colors } from "../colors";
 
 export const lightTheme = {
   primary: colors.stone[800],
-  secondary: colors.blue[200],
+  secondary: colors.red[400],
   background: colors.green[100],
   text: colors.stone[800],
   divider: colors.stone[500],
